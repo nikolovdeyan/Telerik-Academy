c21d36f5f46dec5f77bec888dfa7fcb3724ebc56
nikolovdeyan <deyan.nikolov@gmail.com>
2016-10-27 13:55:34 +0300

    201610271354 | HTML Fundamentals | HW03 HTML Corrections & C# HW03 Complete

---
+++ C# Fundamentals/HOMEWORK/CSharp_03_HW/15.BitSwap/BitSwap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+class BitSwap
+{
+    static void Main()
+    {
+        uint n = uint.Parse(Console.ReadLine());
+        int p = int.Parse(Console.ReadLine());
+        int q = int.Parse(Console.ReadLine());
+        int k = int.Parse(Console.ReadLine());
+
+        for (int i = p; i <= ((p + k) - 1); i++)
+        {
+            uint mask = 1U;
+
+            uint bitP = ((mask << i) & n) >> i;
+            uint bitQ = ((mask << q) & n) >> q;
+
+            n = n & ~(mask << i);
+            n = n & ~(mask << q);
+            n = n | (bitP << q);
+            n = n | (bitQ << i);
+
+            q++;
+
+        }
+
+        Console.WriteLine(n);
+    }
+}
