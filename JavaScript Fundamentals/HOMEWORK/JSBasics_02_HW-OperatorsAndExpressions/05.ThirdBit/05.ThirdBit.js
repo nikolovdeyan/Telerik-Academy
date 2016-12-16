ed05c4495861bebdd1b85c4cac644e7f1e8fbab1
nikolovdeyan <deyan.nikolov@gmail.com>
2016-12-16 09:20:35 +0200

    201612160919 | JavaScript Fundamentals | Fixing an incorrect solution

--- JavaScript Fundamentals/HOMEWORK/JSBasics_02_HW-OperatorsAndExpressions/05.ThirdBit/05.ThirdBit.js
+++ JavaScript Fundamentals/HOMEWORK/JSBasics_02_HW-OperatorsAndExpressions/05.ThirdBit/05.ThirdBit.js
@@ -4,13 +4,9 @@
 function solveProblem(args) {
     var n = +args[0];
 
-    var nBase2 = '0' + ( n >>> 0 ).toString(2);
-    //another way
-    //console.log('0' + n.toString(2));
-
-    var result = nBase2.slice(-3, -2);
+    var nBase2 = '000' + n.toString(2);
+    var result = nBase2.slice(-4, -3);
 
     jsConsole.writeLine(result);
     console.log(result);
 }
-
