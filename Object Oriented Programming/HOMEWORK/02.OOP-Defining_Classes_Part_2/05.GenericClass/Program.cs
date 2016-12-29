e9a16ecdae2d44ffb016ee9d273c8c926a331e22
nikolovdeyan <deyan.nikolov@gmail.com>
2016-12-29 21:35:03 +0200

    201612292134 | Object Oriented Programming | HW02 Problems 5-6

---
+++ Object Oriented Programming/HOMEWORK/02.OOP-Defining_Classes_Part_2/05.GenericClass/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.GenericClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Testing the GenericList
+            // First initialize a GenericList of type string
+            GenericList<string> myTestStrings = new GenericList<string>();
+
+            // Add a couple of strings for the GenericList to hold and print
+            myTestStrings.Add("Winter");
+            myTestStrings.Add("is");
+            myTestStrings.Add("coming!");
+
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("GenericList after Add()s:");
+            Console.WriteLine(myTestStrings);
+            Console.WriteLine("GenericList Size: {0}, Last Element Position: {1}", myTestStrings.Size, myTestStrings.LastElement);
+            Console.WriteLine();
+
+            // Remove the item at position 0 and print again
+            myTestStrings.RemoveAt(0);
+
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("GenericList after Remove()-ing an item:");
+            Console.WriteLine(myTestStrings);
+            Console.WriteLine("GenericList Size: {0}, Last Element Position: {1}", myTestStrings.Size, myTestStrings.LastElement);
+            Console.WriteLine();
+
+            // Insert a new item at position 0 and print again
+            myTestStrings.Insert("TodorHodor", 0);
+
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("GenericList after Insert()-ing an item at position 0:");
+            Console.WriteLine(myTestStrings);
+            Console.WriteLine("GenericList Size: {0}, Last Element Position: {1}", myTestStrings.Size, myTestStrings.LastElement);
+            Console.WriteLine();
+
+            // Lets try to resize the array by inserting some more items
+            myTestStrings.Insert("I", 0);
+            myTestStrings.Insert("guess", 1);
+            myTestStrings.Insert("really", 3);
+
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("GenericList after increasing its size:");
+            Console.WriteLine(myTestStrings);
+            Console.WriteLine("GenericList Size: {0}, Last Element Position: {1}", myTestStrings.Size, myTestStrings.LastElement);
+            Console.WriteLine();
+
+            // Now the list is cleared. The size will return to the default const value
+            myTestStrings.Clear();
+
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("GenericList after using .Clear():");
+            Console.WriteLine(myTestStrings);
+            Console.WriteLine("GenericList Size: {0}, Last Element Position: {1}", myTestStrings.Size, myTestStrings.LastElement);
+            Console.WriteLine();
+
+            // Creating a new list with some numbers
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            GenericList<int> myTestNums = new GenericList<int>(10, arr);
+
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("GenericList is now using integers:");
+            Console.WriteLine(myTestNums);
+            Console.WriteLine("GenericList Size: {0}, Last Element Position: {1}", myTestNums.Size, myTestNums.LastElement);
+            Console.WriteLine();
+
+            // Testing the Find() method now
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("GenericList testing Find(): Where is 8?");
+            Console.WriteLine("Element '8' found at position {0}", myTestNums.Find(8));
+
+
+
+        }
+    }
+}
---
+++ Object Oriented Programming/HOMEWORK/02.OOP-Defining_Classes_Part_2/06.AutoGrow/GenericList.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.AutoGrow
+{
+    public class GenericList<T>
+        where T : IComparable, IComparable<T>
+    {
+        // CONSTANTS
+        private const int DefListSize = 4;
+
+        // FIELDS
+        private int size;
+        private int lastElement;
+        private T[] contents;
+
+        // CONSTRUCTORS
+        // Default constructor initializes a list with 8 items
+        public GenericList()
+            : this(DefListSize)
+        {
+        }
+        public GenericList(int capacity)
+        {
+            this.Size = capacity;
+            this.LastElement = -1;
+            this.Contents = new T[capacity];
+        }
+        public GenericList(int capacity, T[] items)
+        {
+            this.Size = capacity;
+            this.LastElement = items.Length - 1;
+            this.Contents = items;
+        }
+
+        // PROPERTIES
+        public int Size
+        {
+            get { return this.size; }
+            set { this.size = (value > DefListSize) ? value : DefListSize; }
+        }
+        public int LastElement
+        {
+            get { return this.lastElement; }
+            set { this.lastElement = value; }
+        }
+        public int Count
+        {
+            get { return this.LastElement + 1; }
+        }
+        public T[] Contents
+        {
+            get { return this.contents; }
+            set { this.contents = value; }
+        }
+        // Indexer property -- Accessing an element by index
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < this.Size)
+                {
+                    return this.contents[index];
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            set
+            {
+                if (index >= 0 && index < this.Size)
+                {
+                    this.contents[index] = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        // METHODS
+        // Adding an element
+        public void Add(T element)
+        {
+            this.LastElement++;
+
+            // Check if resizing is needed
+            if (this.LastElement == this.Size)
+            {
+                this.ResizeArray("grow");
+            }
+
+            this.Contents[this.LastElement] = element;
+        }
+
+        // Removing an element by index
+        public void RemoveAt(int pos)
+        {
+            this.LastElement--;
+
+            if (pos > this.Contents.Length || pos < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+                int tmpIndex = 1;
+                for (int i = 0; i < this.Contents.Length; i++)
+                {
+                    if (i != pos)
+                    {
+                        this.Contents[tmpIndex - 1] = this.Contents[i];
+                        tmpIndex++;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+            // Check if shrinking of the array is needed
+            if (this.LastElement < (this.Size / 2) && (this.Size / 2 >= DefListSize))
+            {
+                this.ResizeArray("shrink");
+            }
+        }
+
+        // Inserting element at position
+        public void Insert(T element, int pos)
+        {
+
+            if (pos < 0 || pos > this.LastElement)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                this.LastElement++;
+                if (this.LastElement == 0)
+                {
+                    this.Add(element);
+                }
+                else
+                {
+                    T tmp = this[pos];
+                    this[pos] = element;
+                    for (int i = pos + 1; i < this.LastElement + 1; i++)
+                    {
+                        // Check if resizing is needed
+                        if (this.LastElement == this.Size)
+                        {
+                            this.ResizeArray("grow");
+                        }
+
+                        T tmpItem = this[i];
+                        this[i] = tmp;
+                        tmp = tmpItem;
+                    }
+                }
+                
+            }
+        }
+
+        // Clearing the list
+        public void Clear()
+        {
+            this.Contents = new T[DefListSize];
+            this.LastElement = -1;
+            this.ResizeArray("shrink");
+        }
+
+        // Finding an element by its value
+        public int Find(T element)
+        {
+            if (this.LastElement == -1)
+            {
+                return -1;
+            }
+            else
+            {
+                for (int i = 0; i <= this.LastElement; i++)
+                {
+                    if (this[i].ToString() == element.ToString())
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+        }
+
+        // Resizing the array
+        private void ResizeArray(string operation)
+        {
+            // If growing: 
+            if (operation == "grow")
+            {
+                this.Size *= 2;
+                var resizedArr = new T[this.Size];
+                this.Contents.CopyTo(resizedArr, 0);
+                this.Contents = resizedArr;
+                
+            }
+            // If shrinking:
+            else if (operation == "shrink")
+            {
+                this.Size /= 2;
+                var resizedArr = new T[this.Size];
+                this.Contents.CopyTo(resizedArr, 0);
+                this.Contents = resizedArr;
+            }
+        }
+
+        // ToString Override
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (this.Count > 0)
+            {
+                for (int i = 0; i < this.Count; i++)
+                {
+                    sb.Append(this[i])
+                      .Append(" ");
+                }
+            }
+            // Trimming the end space here
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
